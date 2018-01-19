2f96c6561cd0927e65a13b0f0100197b35f3b7cf
Domnik Mack <domnikmack@gmail.com>
2018-01-19 17:06:03 -0500

    first setup

---
+++ app.js
@@ -0,0 +1,27 @@
+'user strict';
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+
+// logging middleware
+app.use(morgan('dev'));
+
+//body parsing
+app.use(bodyParser.urlencoded({extended: true}));
+
+//static route
+app.use(express.static(path.join(__dirname + '/public')));
+
+//template engine
+var env = nunjucks.configure('views', {noCache: true});
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+
+app.get('/', function(res, req) {
+  'index'
+});
+
+app.listen(3000);
