7eb2a825e574c098fcd9e3c0256bc3d1f59fa472
Domnik Mack <domnikmack@gmail.com>
2018-01-22 14:54:36 -0500

    hooks done

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const app = express();
-const router = express.Router;
+const router = express.Router();
 
 module.exports = router;
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const models = require('../models');
+const Page = models.Page;
+const User = models.User;
 
 router.get('/', function(req, res, next) {
   res.redirect('/');
@@ -8,7 +11,13 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   // res.send('This was a post request');
-  res.json(req.body);
+    var page = Page.build({
+      title: req.body.name,
+      content: req.body.content
+    });
+  // res.json(req.body);
+  page.save()
+  .then(() => res.redirect('/'));
 });
 
 router.get('/add', function(req,res, next) {
